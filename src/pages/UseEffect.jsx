d0d92a37c246b078d2415c800751212519febe30
Utsav Singh <89920027+Utsav-Singh-01@users.noreply.github.com>
2023-12-03 06:01:33 +0530

    made pages folder for future router setup

---
+++ src/pages/UseEffect.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const UseEffect = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+      console.log("Hey buddy!")
+  
+      return console.log("Buy Buddy!");
+    }, [count]);
+  
+    return (
+      <>
+        <div>Count: {count}</div>
+        <button onClick={() => setCount((count) => count + 1)}>Increment</button>
+        <button onClick={() => setCount((count) => count - 1)}>Decrement</button>
+      </>
+    );
+}
+
+export default UseEffect
